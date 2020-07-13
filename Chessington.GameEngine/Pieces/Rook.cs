245035622c3a247b835dfb82570349d5ca0e7588
Zhian Zhou <azh2709@gmail.com>
2020-07-13 21:15:34 +0800

    Rook cannot move past other pieces

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -12,13 +12,35 @@ namespace Chessington.GameEngine.Pieces
         {
             var availableMoves = new List<Square>();
             var currentSquare = board.FindPiece(this);
-            for (var i = 0; i < 8; i++)
-                availableMoves.Add(Square.At(currentSquare.Row, i));
-            for (var i = 0; i < 8; i++)
-                availableMoves.Add(Square.At(i, currentSquare.Col));
-
-            //Get rid of our starting location.
-            availableMoves.RemoveAll(s => s == Square.At(4, 4));
+            for (var i = 1; currentSquare.Col+i < 8; i++)
+            {
+                var square = new Square(currentSquare.Row, currentSquare.Col + i);
+                if (board.GetPiece(square) != null)
+                    break;
+                availableMoves.Add(square);
+            }
+            for (var i = 1; currentSquare.Col-i >= 0; i++)
+            {
+                var square = new Square(currentSquare.Row, currentSquare.Col - i);
+                if (board.GetPiece(square) != null)
+                    break;
+                availableMoves.Add(square);
+            }
+            for (var i = 1; currentSquare.Row+i < 8; i++)
+            {
+                var square = new Square(currentSquare.Row + i, currentSquare.Col);
+                if (board.GetPiece(square) != null)
+                    break;
+                availableMoves.Add(square);
+            }
+            for (var i = 1; currentSquare.Row-i >= 0; i++)
+            {
+                var square = new Square(currentSquare.Row - i, currentSquare.Col);
+                if (board.GetPiece(square) != null)
+                    break;
+                availableMoves.Add(square);
+            }
+            
             return availableMoves;
         }
     }
