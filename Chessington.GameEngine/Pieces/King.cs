60805c5a1adfd08749535b47939ea29a91545cc1
Zhian Zhou <azh2709@gmail.com>
2020-07-13 20:56:45 +0800

    Add King movement

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,17 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            
+            // diagonal movements
+            for (var i = -1; i <= 1; i++)
+            for (var j = -1; j <= 1; j++)
+                availableMoves.Add(Square.At(currentSquare.Row + i, currentSquare.Col + j));
+
+            //Get rid of our starting location.
+            availableMoves.RemoveAll(s => s == currentSquare);
+            return availableMoves;
         }
     }
 }
