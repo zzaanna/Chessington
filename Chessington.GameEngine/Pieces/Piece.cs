c8d6390d49f6a7fef0613d99b3f422139e5a271b
Zhian Zhou <azh2709@gmail.com>
2020-07-13 17:59:31 +0800

    Red-01 complete Piece.getAvailableMoves()

--- Chessington.GameEngine/Pieces/Piece.cs
+++ Chessington.GameEngine/Pieces/Piece.cs
@@ -13,7 +13,18 @@ namespace Chessington.GameEngine.Pieces
 
         public Player Player { get; private set; }
 
-        public abstract IEnumerable<Square> GetAvailableMoves(Board board);
+        public virtual IEnumerable<Square> GetAvailableMoves(Board board)
+        {
+            IEnumerable<Square> availableSquares = Enumerable.Empty<Square>();
+            for (var row = 0; row < GameSettings.BoardSize; row++)
+                for (var col = 0; col < GameSettings.BoardSize; col++)
+                {
+                    var currentSquare = new Square(row, col);
+                    if (board.GetPiece(currentSquare) == null)
+                        availableSquares = availableSquares.Concat(new []{currentSquare});
+                }
+            return availableSquares;
+        }
 
         public void MoveTo(Board board, Square newSquare)
         {
