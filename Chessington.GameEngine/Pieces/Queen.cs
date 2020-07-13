fd82ff77740acce1789ef05d865d3e8872c38da9
Zhian Zhou <azh2709@gmail.com>
2020-07-13 20:42:14 +0800

    Add Queen movements

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,25 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            
+            // lateral movements
+            for (var i = 0; i < 8; i++)
+                availableMoves.Add(Square.At(currentSquare.Row, i));
+            for (var i = 0; i < 8; i++)
+                availableMoves.Add(Square.At(i, currentSquare.Col));
+            
+            // diagonal movements
+            for (var i = 0; i < 8; i++)
+                availableMoves.Add(Square.At(i, i - currentSquare.Row + currentSquare.Col));
+            for (var i = 1; i < 8; i++)
+                availableMoves.Add(Square.At(i, currentSquare.Row + currentSquare.Col - i));
+
+            //Get rid of our starting location.
+            availableMoves.RemoveAll(s => s == currentSquare);
+            
+            return availableMoves;
         }
     }
 }
