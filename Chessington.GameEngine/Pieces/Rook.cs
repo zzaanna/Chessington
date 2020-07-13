dd926c0dea883bf5519871dbc9bc445a506fb62a
Zhian Zhou <azh2709@gmail.com>
2020-07-13 20:05:23 +0800

    Add rook moves

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,16 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            for (var i = 0; i < 8; i++)
+                availableMoves.Add(Square.At(currentSquare.Row, i));
+            for (var i = 0; i < 8; i++)
+                availableMoves.Add(Square.At(i, currentSquare.Col));
+
+            //Get rid of our starting location.
+            availableMoves.RemoveAll(s => s == Square.At(4, 4));
+            return availableMoves;
         }
     }
 }
