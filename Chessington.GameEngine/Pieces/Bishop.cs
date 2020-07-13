64108e9ef73644bd4b6b9fca3993c2ed45ad7349
Zhian Zhou <azh2709@gmail.com>
2020-07-13 21:38:24 +0800

    Bishop cannot move through other pieces

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -9,18 +9,51 @@ namespace Chessington.GameEngine.Pieces
         public Bishop(Player player)
             : base(player) { }
 
+        private bool canMove(Square square, Board board)
+        {
+            if (square.Row < 0 || square.Row > 7)
+                return false;
+            if (square.Col < 0 || square.Col > 7)
+                return false;
+            if (board.GetPiece(square) != null)
+                return false;
+            return true;
+        }
+
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             var availableMoves = new List<Square>();
             var currentSquare = board.FindPiece(this);
             
-            for (var i = 0; i < 8; i++)
-                availableMoves.Add(Square.At(i, i - currentSquare.Row + currentSquare.Col));
             for (var i = 1; i < 8; i++)
-                availableMoves.Add(Square.At(i, currentSquare.Row + currentSquare.Col - i));
-
-            //Get rid of our starting location.
-            availableMoves.RemoveAll(s => s == currentSquare);
+            {
+                var square = new Square(currentSquare.Row + i, currentSquare.Col + i);
+                if (!canMove(square, board))
+                    break;
+                availableMoves.Add(square);
+            }
+            for (var i = 1; i < 8; i++)
+            {
+                var square = new Square(currentSquare.Row + i, currentSquare.Col - i);
+                if (!canMove(square, board))
+                    break;
+                availableMoves.Add(square);
+            }
+            for (var i = 1; i < 8; i++)
+            {
+                var square = new Square(currentSquare.Row - i, currentSquare.Col - i);
+                if (!canMove(square, board))
+                    break;
+                availableMoves.Add(square);
+            }
+            for (var i = 1; i < 8; i++)
+            {
+                var square = new Square(currentSquare.Row - i, currentSquare.Col + i);
+                if (!canMove(square, board))
+                    break;
+                availableMoves.Add(square);
+            }
+            
             return availableMoves;
         }
     }
